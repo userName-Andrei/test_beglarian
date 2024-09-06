f13f6040657964cb5e160bf1ca0e87562fd85247
Andrei Zhigachev <andrei_zhigachev@bk.ru>
2024-09-06 13:53:06 +0300

    tes-2 update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,35 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import CrownIcon from "@/icons/CrownIcon";
+import FireIcon from "@/icons/FireIcon";
+import UiFlexBox from "@/components/UI/UiFlexBox/UiFlexBox";
+import HeaderIconBox from "@/components/HeaderIconBox/HeaderIconBox";
+import LessonBlock from "@/components/LessonBlock/LessonBlock";
+import LessonAdviceNavigation from "@/components/LessonAdviceNavigation/LessonAdviceNavigation";
+import LessonNavigation from "@/components/LessonNavigation/LessonNavigation";
+import Navigation from "@/components/Navigation/Navigation";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className={styles.content}>
+      <UiFlexBox
+        className={styles.header}
+        justify="space-between"
+        align="center"
+      >
+        <p className={styles.text}>Ray Driving Theory</p>
+        <UiFlexBox gap={8}>
+          <HeaderIconBox iconJSX={<CrownIcon />} />
+          <HeaderIconBox iconJSX={<FireIcon />} text={13} />
+        </UiFlexBox>
+      </UiFlexBox>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <LessonBlock name="Learning" category="Category В" />
+
+      <LessonAdviceNavigation />
+
+      <LessonNavigation />
+
+      <Navigation />
+    </main>
   );
 }
