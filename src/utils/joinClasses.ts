7d0a106fedd13a649dd927d6c37b1c3b7752c684
Andrey Zhuravel <andrey.zhuravel.01.23.94@gmail.com>
2024-09-06 18:46:59 +0300

    test-8 add joinclases descriptions

--- src/utils/joinClasses.ts
+++ src/utils/joinClasses.ts
@@ -1,3 +1,20 @@
-export function joinClasses(...classname: Array<string | undefined>): string {
-  return classname.filter((name) => !!name).join(" ");
+/**
+ *
+ * @param {[string| undefined]} className an array of strings that need to be combined
+ * @returns {string} A string with class names
+ * @description This function is used to combine several classes into 1 line with separation to pass them to className
+ * @example 
+ * const Components: FC<ComponentsProps> = ({ children }) => {
+  return (
+    <div
+      className={joinClasses(class1, class2, class3)}
+    >
+      {children}
+    </div>
+  );
+};
+ */
+
+export function joinClasses(...className: Array<string | undefined>): string {
+  return className.filter((name) => !!name).join(" ");
 }
