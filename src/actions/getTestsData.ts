80b1e7b3ddcf952d8b03ae096e3a866f825417c9
Andrey Zhuravel <andrey.zhuravel.01.23.94@gmail.com>
2024-09-06 19:12:40 +0300

    test-8 add get test data gescriptions

--- src/actions/getTestsData.ts
+++ src/actions/getTestsData.ts
@@ -1,3 +1,9 @@
-export async function getTestsData() {
+/**
+ * @description Function for getting data from a json file
+ * @returns {Promise<any>} Returns a promise of type any
+ * @example
+ * getTestsData().then(data => console.log(data))
+ */
+export async function getTestsData(): Promise<any> {
   return fetch("mock.json").then((res) => res.json());
 }
