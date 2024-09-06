07cdc4bf06675568802520941f7f2f3acfe16d5b
Andrei Zhigachev <andrei_zhigachev@bk.ru>
2024-09-06 10:02:12 +0300

    tes-2 added link at LessonBlock

--- src/components/LessonBlock/LessonBlock.tsx
+++ src/components/LessonBlock/LessonBlock.tsx
@@ -7,6 +7,7 @@ import ChevronRightIcon from "@/icons/ChevronRightIcon";
 
 import styles from "./LessonBlock.module.css";
 import LearningProgress from "../LearningProgress/LearningProgress";
+import Link from "next/link";
 
 interface LessonBlockProps {
   name: string;
@@ -17,23 +18,25 @@ const LessonBlock: FC<LessonBlockProps> = ({ name, category }) => {
   return (
     <UiSection className={styles.block} size="big">
       <UiFlexBox className={styles.lesson} col gap={16}>
-        <UiFlexBox justify="space-between">
-          <UiFlexBox gap={16} grow>
-            <IconBoxRound
-              className={styles.capIcon}
-              iconJSX={<CapIcon />}
-              width={58}
-              backgroundColor="#FFE3DD"
-              backgroundOpacity={0.5}
-            />
-            {/* Можно было бы сделать align: center, но тогда при переполнении иконки будут в центре */}
-            <UiFlexBox className={styles.lesson_text} col>
-              <p className={styles.lesson_name}>{name}</p>
-              <p className={styles.lesson_category}>{category}</p>
+        <Link href={"/tests"}>
+          <UiFlexBox justify="space-between">
+            <UiFlexBox gap={16} grow>
+              <IconBoxRound
+                className={styles.capIcon}
+                iconJSX={<CapIcon />}
+                width={58}
+                backgroundColor="#FFE3DD"
+                backgroundOpacity={0.5}
+              />
+              {/* Можно было бы сделать align: center, но тогда при переполнении иконки будут в центре */}
+              <UiFlexBox className={styles.lesson_text} col>
+                <p className={styles.lesson_name}>{name}</p>
+                <p className={styles.lesson_category}>{category}</p>
+              </UiFlexBox>
             </UiFlexBox>
+            <ChevronRightIcon className={styles.chevron} />
           </UiFlexBox>
-          <ChevronRightIcon className={styles.chevron} />
-        </UiFlexBox>
+        </Link>
         <LearningProgress />
       </UiFlexBox>
     </UiSection>
